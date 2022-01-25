58f75a7bf1075d31bf4e33a467383249b8049073
Marcelo <marcelodpaternostro@gmail.com>
2022-01-25 17:35:45 -0300

    feat: handle errors with remix error boundaries

--- app/root.tsx
+++ app/root.tsx
@@ -3,17 +3,12 @@ import {
 	LinksFunction,
 	LiveReload,
 	Meta,
-	MetaFunction,
 	Outlet,
+	useCatch,
 } from "remix";
-
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
 import globalLargeStylesUrl from "./styles/global-large.css";
-
-export const meta: MetaFunction = () => {
-	return { title: "Remix Todos App" };
-};
 
 export const links: LinksFunction = () => {
 	return [
@@ -34,19 +29,59 @@ export const links: LinksFunction = () => {
 	];
 };
 
-export default function App() {
+function Document({
+	children,
+	title = `Remix Todos App`,
+}: {
+	children: React.ReactNode;
+	title?: string;
+}) {
 	return (
 		<html lang="en">
 			<head>
 				<meta charSet="utf-8" />
+				<title>{title}</title>
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<Meta />
 				<Links />
 			</head>
 			<body>
-				<Outlet />
+				{children}
 				{process.env.NODE_ENV === "development" ? <LiveReload /> : null}
 			</body>
 		</html>
+	);
+}
+
+export default function App() {
+	return (
+		<Document>
+			<Outlet />
+		</Document>
+	);
+}
+
+export function CatchBoundary() {
+	const caught = useCatch();
+
+	return (
+		<Document title={`${caught.status} ${caught.statusText}`}>
+			<div className="error-container">
+				<h1>
+					{caught.status} {caught.statusText}
+				</h1>
+			</div>
+		</Document>
+	);
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+	return (
+		<Document title="Something went wrong!">
+			<div className="error-container">
+				<h1>App Error</h1>
+				<pre>{error.message}</pre>
+			</div>
+		</Document>
 	);
 }
