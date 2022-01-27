55b2bdb8a94c327f3a6429a84aa8b58032d6d8bd
Marcelo <marcelodpaternostro@gmail.com>
2022-01-27 18:57:43 -0300

    feat: be logged in to proceed in app

--- app/root.tsx
+++ app/root.tsx
@@ -2,13 +2,17 @@ import {
 	Links,
 	LinksFunction,
 	LiveReload,
+	LoaderFunction,
 	Meta,
 	Outlet,
+	redirect,
 	useCatch,
+	useLoaderData,
 } from "remix";
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
 import globalLargeStylesUrl from "./styles/global-large.css";
+import { gql, GraphQLClient } from "graphql-request";
 
 export const links: LinksFunction = () => {
 	return [
@@ -53,9 +57,86 @@ function Document({
 	);
 }
 
+// replace this with codegen
+interface QueryResult {
+	whoAmI: {
+		id: string;
+		username: string;
+		currentHashedRefreshToken: string;
+		createdAt: string;
+		updatedAt: string;
+	};
+}
+
+// replace this with codegen
+const whoami = gql`
+	query whoami {
+		whoAmI {
+			id
+			username
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+interface GraphQLError {
+	response: {
+		errors: {
+			message: string;
+		}[];
+	};
+}
+
+function isGraphQLError(error: unknown): error is GraphQLError {
+	return typeof error === "object" && error !== null && "response" in error;
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+	if (typeof process.env.SERVER_ENDPOINT !== "string") {
+		throw new Error("SERVER_ENDPOINT must be set");
+	}
+	const url = new URL(request.url, process.env.SERVER_ENDPOINT);
+	const isLoginPath = url.pathname === "/login";
+	const client = new GraphQLClient(`${process.env.SERVER_ENDPOINT}/graphql`);
+	try {
+		const headers = new Headers();
+		headers.append("Cookie", request.headers.get("cookie") || "");
+		const data = await client.request<QueryResult>(whoami, {}, headers);
+		if (isLoginPath && data.whoAmI) {
+			// you are logged in, so you are redirected to the app
+			return redirect("/todos");
+		}
+		return data;
+	} catch (error) {
+		if (typeof error === "string") {
+			throw new Response(error, { status: 500 });
+		} else if (typeof error === "object" && error !== null) {
+			if (isGraphQLError(error)) {
+				if (error.response.errors[0].message === "Unauthorized") {
+					if (!isLoginPath) {
+						// you are not logged in, so you are redirected to the login page
+						return redirect("/login");
+					}
+					// we don't want to throw here, because we know the user is in
+					// the login page and not authenticated
+					return null;
+				}
+			}
+		}
+		throw new Error("Unknown error");
+	}
+};
+
 export default function App() {
+	const data = useLoaderData<QueryResult | null>();
 	return (
 		<Document>
+			{data ? (
+				<nav>
+					<span>{`Hello ${data.whoAmI.username}`}</span>
+				</nav>
+			) : null}
 			<Outlet />
 		</Document>
 	);
